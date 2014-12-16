3578a3fd827831f0c1e5f2990f065db9025e7aa9
Jason Paryani <github@jparyani.com>
2014-12-15 18:10:20 -0800

    Fix up test script

--- test.py
+++ test.py
@@ -1,19 +1,27 @@
 import capnp
 import sys
 sys.path.append('/opt/sandstorm/latest/usr/include/sandstorm/')
+import web_session_capnp
 import hack_session_capnp
 import os
 import requests
 
 
+class WebSession(web_session_capnp.WebSession.Server):
+  def get(self, path, _context, **kwargs):
+    resp = requests.get('http://google.com' + path)
+    _context.results.from_dict({'content': {'body': {'bytes': resp.text.encode(encoding='UTF-8')}}})
+
+
 class Server(hack_session_capnp.HackSessionContext.Server):
   def send(self, *args, **kwargs):
     print args, kwargs
+
   def getApiEndpoint(self, *args, **kwargs):
     return 'http://google.com'
-  def httpGet(self, url, **kwargs):
-    resp = requests.get(url)
-    return (resp.headers['content-type'], resp.text.encode(encoding='UTF-8'))
+
+  def getUIViewForToken(self, token, **kwargs):
+    return WebSession()
 
 if os.path.exists('/tmp/sandstorm-api'):
   os.unlink('/tmp/sandstorm-api')
